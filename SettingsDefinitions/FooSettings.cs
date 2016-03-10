ca2e2a224c6f6fe683c6fe70f457258f5a037ead
Guilherme Niero <guilherme@equipe.com.br>
2016-03-10 17:24:07 -0300

    First commit containing some definitions

---
+++ SettingsDefinitions/FooSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsTest
+{
+    public class FooSettings : ConfigurationElement
+    {
+        [ConfigurationProperty("Name", IsRequired = true)]
+        public string Name
+        {
+            get
+            {
+                return this["Name"] as string;
+            }
+        }
+
+        [ConfigurationProperty("Address", IsRequired = true)]
+        public string Address
+        {
+            get
+            {
+                return this["Address"] as string;
+            }
+        }
+    }
+}
---
+++ SettingsTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using SettingsTest.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
